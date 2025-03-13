10f13205eff8051097a893fda1306e3c775773a0
Hiran Greening <hg113@students.waikato.ac.nz>
2025-03-13 21:45:30 +1300

    commented test java

--- A1/test.java
+++ A1/test.java
@@ -1,12 +1,33 @@
+ // ID: 1522172
+// Name: Hiran Greening
+ 
+ /**
+  * This is a test class to test the Heap class
+  */
 public class test {
+
+    /**
+     * Main method to test the Heap class
+     * @param args
+     */
     public static void main(String[] args) {
+
+        // array of test data
         String[] testData = {"apple", "orange", "banana", "grape", "kiwi"};
+
+        // Create a new heap
         Heap heap = new Heap(testData.length);
+
+        // Insert test data into the heap
         heap.heapify(testData);
+
+        // Sort the heap
         heap.heapsort();
 
         // Use the getter to access sorted data
         for (String s : heap.getHeapArray()) {
+
+            // Print the sorted data
             System.out.println(s);
         }
     }
